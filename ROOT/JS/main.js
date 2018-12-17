1d82463182f5a9006e890381eb37acd8ff858040
AstellJ11 <45542564+AstellJ11@users.noreply.github.com>
2018-12-17 16:18:08 +0000

    Working on the scrollingImages

--- ROOT/JS/main.js
+++ ROOT/JS/main.js
@@ -1,27 +1,27 @@
 window.onload = function()
 {
 	console.log('ready');
-}
+};
+
+document.addEventListener("DOMContentLoaded", function(event) { 
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slideshowFade");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length};
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
@@ -31,6 +31,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+});
 
 
 
